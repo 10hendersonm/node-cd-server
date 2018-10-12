0798b3a87db8f5534e1e81fecfe37e4b2f69f19c
Marty Henderson <10hendersonm@gmail.com>
2018-10-12 13:50:33 -0500

    added build command

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import {Router} from 'express'
 
 const router = Router()
 
-router.post('/test', (req, res) => {
+router.post('/cd', (req, res) => {
   console.log(req.body)
   res.json({
     message: 'Hello World',
