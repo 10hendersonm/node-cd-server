3df7d3592d083a58cfb8e36ff7ba9d2774b95dbb
Marty Henderson <10hendersonm@gmail.com>
2018-10-12 15:04:44 -0500

    added log

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ const router = Router()
 
 router.post('/cd', (req, res) => {
   const push = new PushModel(req.body)
+  console.log(push)
 
   if (push.pusherName !== '10hendersonm') {
     res.status(401).send('Invalid pusher')
