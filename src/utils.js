85bd2d554263fc4a3ab12d46d869b1bfb42eb220
Marty Henderson <10hendersonm@gmail.com>
2018-10-13 03:19:26 -0500

    multi-stage dockerfile

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,15 @@
 export const createDockerfile = ({projectName, commitId, cloneUrl, buildSteps}) => {
-  return `FROM ubuntu
+  return `
+FROM node as builder
 WORKDIR /build
-RUN apt-get update && apt-get upgrade -y
-RUN apt-get install nodejs git npm docker -y
 RUN git clone ${cloneUrl} /build
 ${buildSteps.map((step) =>
-    `RUN ${step}`
-  ).join('\r\n')}
+  `RUN ${step}`
+).join('\r\n')}
+
+FROM docker
+WORKDIR /build
+COPY --from=builder /build .
 CMD docker build -t ${projectName}-${commitId} .
 `
 }
