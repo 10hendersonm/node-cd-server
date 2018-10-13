db30dc249d6d012eeadfbb4b51a25fd3573e8fec
Marty Henderson <10hendersonm@gmail.com>
2018-10-12 21:53:37 -0500

    using clone-build in dockerfile

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export const createDockerfile = ({projectName, commitId, cloneUrl, buildSteps}) => {
-  return `FROM node
-RUN git clone ${cloneUrl} build
+  return `FROM clone-build
 WORKDIR /build
+RUN git clone ${cloneUrl} /build
 ${buildSteps.map((step) => `RUN ${step}`).join('\r\n')}
 RUN docker build -t ${projectName}-${commitId} .`
 }
