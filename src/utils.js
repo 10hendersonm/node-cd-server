42b2ee30baddd4775210b06dcda06dca4f7d3e7f
Marty Henderson <10hendersonm@gmail.com>
2018-10-12 21:56:36 -0500

    moved it all into one dockerfile

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,10 @@
 export const createDockerfile = ({projectName, commitId, cloneUrl, buildSteps}) => {
-  return `FROM clone-build
+  return `FROM debian
 WORKDIR /build
+RUN apt-get update && apt-get upgrade
+RUN apt-get install nodejs yarn git docker -y
 RUN git clone ${cloneUrl} /build
 ${buildSteps.map((step) => `RUN ${step}`).join('\r\n')}
+RUN docker build -t ${projectName}-${commitId} .
 `
 }
