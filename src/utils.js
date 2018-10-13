98121db9e7bb8a06bbb956b1d8c6ccc9ebbeda0d
Marty Henderson <10hendersonm@gmail.com>
2018-10-13 03:00:57 -0500

    yes flags to apt-get

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,8 @@
 export const createDockerfile = ({projectName, commitId, cloneUrl, buildSteps}) => {
   return `FROM ubuntu
 WORKDIR /build
-RUN apt-get update && apt-get upgrade
-RUN apt-get install nodejs git npm docker
+RUN apt-get update && apt-get upgrade -y
+RUN apt-get install nodejs git npm docker -y
 RUN git clone ${cloneUrl} /build
 ${buildSteps.map((step) =>
     `RUN ${step}`
