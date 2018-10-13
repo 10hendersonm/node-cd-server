6900137690b3c67d2dbc9fbb26d7a9ce404f1210
Marty Henderson <10hendersonm@gmail.com>
2018-10-12 23:10:45 -0500

    moved dockerize copy

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export const createDockerfile = ({projectName, commitId, cloneUrl, buildSteps}) => {
   return `FROM node
 WORKDIR /build
-COPY dockerize.js /tmp
+COPY ./build/dockerize.js /tmp
 RUN git clone ${cloneUrl} /build
 ${buildSteps.map((step) => `RUN ${step}`).join('\r\n')}
 CMD node /tmp/dockerize.js ${projectName}-${commitId}
