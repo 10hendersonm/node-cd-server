ecad5d518b18ace1089a26aee640da1890f64329
Marty Henderson <10hendersonm@gmail.com>
2018-10-12 22:09:59 -0500

    removing cmdtest to get rid of wrong yarn version

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,7 @@ export const createDockerfile = ({projectName, commitId, cloneUrl, buildSteps}) 
   return `FROM debian
 WORKDIR /build
 RUN apt-get update && apt-get upgrade
+RUN apt-get remove cmdtest
 RUN apt-get install nodejs yarn git docker -y
 RUN git clone ${cloneUrl} /build
 ${buildSteps.map((step) => `RUN ${step}`).join('\r\n')}
