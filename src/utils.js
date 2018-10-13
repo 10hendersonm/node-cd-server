2b8978903c5552b456e9914c0f40b263dad06017
Marty Henderson <10hendersonm@gmail.com>
2018-10-13 02:59:08 -0500

    moved back to a ubuntu model

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,8 @@
 export const createDockerfile = ({projectName, commitId, cloneUrl, buildSteps}) => {
-  return `FROM node
+  return `FROM ubuntu
 WORKDIR /build
+RUN apt-get update && apt-get upgrade
+RUN apt-get install nodejs git npm docker
 RUN git clone ${cloneUrl} /build
 ${buildSteps.map((step) =>
     `RUN ${step}`
