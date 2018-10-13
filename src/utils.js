604521446bc5fdcfdc87c12d3b44b1b0cc8e45c7
Marty Henderson <10hendersonm@gmail.com>
2018-10-13 00:08:15 -0500

    put docerize back in utils dockerfile gen

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,11 @@
 export const createDockerfile = ({projectName, commitId, cloneUrl, buildSteps}) => {
   return `FROM node
 WORKDIR /build
+COPY dockerize.js /tmp/
 RUN git clone ${cloneUrl} /build
 ${buildSteps.map((step) =>
     `RUN ${step}`
   ).join('\r\n')}
+CMD node /tmp/dockerize.js
 `
 }
