4be727f6c8926350c10654c226b80896b7dc80c9
Marty Henderson <10hendersonm@gmail.com>
2018-10-12 22:16:01 -0500

    fixing yarn install

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,9 @@
 export const createDockerfile = ({projectName, commitId, cloneUrl, buildSteps}) => {
   return `FROM debian
 WORKDIR /build
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 RUN apt-get update && apt-get upgrade
-RUN apt-get remove cmdtest
 RUN apt-get install nodejs yarn git docker -y
 RUN git clone ${cloneUrl} /build
 ${buildSteps.map((step) => `RUN ${step}`).join('\r\n')}
